82f74f6265e7ed84fecaafecbd73937354f3470c
Lucas Camargo <lucaspescadordecamargo@gmail.com>
2025-04-14 21:53:39 -0300

    Adicionando hooks

---
+++ cypress/e2e/cart.cy.js
@@ -0,0 +1,20 @@
+describe('Cart', () => {
+
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+    })
+
+    it('Adicionar produto ao carrinho com SUCESSO', () => {
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+        cy.get('.shopping_cart_badge')
+          .should('be.visible')
+          .and('have.text', '1')
+
+        cy.get('#shopping_cart_container').click()
+        cy.contains('Sauce Labs Backpack').should('be.visible')
+    })
+})
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,18 @@
 describe('Login', () => {
-    it('Login com SUCESSO', () => {
+
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/')
+    })
+
+    it('Login com SUCESSO', () => {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
         cy.url().should('equal', 'https://www.saucedemo.com/inventory.html')
+        //cy.screenshot('login sucesso')
     })
 
     it('Login com FALHA', () => {
-        cy.visit('https://www.saucedemo.com/')
         cy.get('#user-name').type('login_invalido')
         cy.get('#password').type('senha_invalida')
         cy.get('#login-button').click()
