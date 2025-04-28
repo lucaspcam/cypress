377ae080bdbe972dc7f837f2e26d3e3ff8614994
Lucas Camargo <lucaspescadordecamargo@gmail.com>
2025-04-27 21:54:09 -0300

    Teste remover produto carrinho

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -17,4 +17,11 @@ describe('Cart', () => {
         cy.get('#shopping_cart_container').click()
         cy.contains('Sauce Labs Backpack').should('be.visible')
     })
+
+    it('Remover produtos do carrinho com SUCESSO ', () => {
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('.shopping_cart_badge')
+          .should('be.visible')
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+    })
 })
